c9466c3c4c3514530ac98ac325a9e001c2973bb8
Jamz Tang <jamz@jamztang.com>
2013-12-31 19:34:28 +0800

    Initial Commit

---
+++ customTransition/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  customTransition
+//
+//  Created by Jamz Tang on 31/12/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ customTransition/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  customTransition
+//
+//  Created by Jamz Tang on 31/12/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
